c7dca6289c443f3faccccd7f58fc17c59cb751a4
Michael <mzhu105@gmail.com>
2018-01-25 18:37:21 -0800

    Set up mongoDB

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
-const express = require('express');
-require('./services/passport');
+const express = require("express");
+require("./services/passport");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-require('./routes/authRoutes')(app);
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
