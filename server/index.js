67538cc929a99c7bf8228646298346b7d4b624f0
Michael <mzhu105@gmail.com>
2018-02-03 17:41:40 -0800

    Set up cookie session

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
+const cookieSession = require('cookie-session');
 require("./models/User"); // load user model
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // cookie lasts 30 days
+    keys: [keys.cookieKey]
+  })
+);
 
 require("./routes/authRoutes")(app);
 
